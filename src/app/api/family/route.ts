b9b0431823baf0fb188a1fb510f0fc02b9314c10
Dibyan <dibyan@restworld.it>
2023-08-20 14:07:45 +0545

    add content

--- src/app/api/family/route.ts
+++ src/app/api/family/route.ts
@@ -1,85 +1,66 @@
-// import type { NextApiRequest, NextApiResponse } from 'next';
-// // import fs from 'fs';
-// import { type NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
-// import { ResponseError, Root } from '@/app/interfaces';
-// import connectToDatabase from '@/app/config/db'
-// // import PersonModel from '@/app/models/Person-old';
+import connectDB from "@/lib/connect-db";
+import { createTodo, getTodos } from "@/lib/todo-db";
+import { createErrorResponse } from "@/lib/utils";
 
-// // Fake users data
-// // import data from '../../../../public/db.json';
+export async function GET(request: NextRequest) {
+  try {
+    await connectDB();
 
-// export async function GET(_req: Request, res: NextApiResponse<Root[]>) {
-//   const { db } = await connectToDatabase();
-//   // const collection = db.collection('Person');
-//   const familyData = await db.collection('Person').find({}).toArray();
-//   console.log({familyData})
-//   return NextResponse.json(familyData);
-// }
-// export async function POST(_req: NextRequest) {
-//   const filePath = './public/db.json';
-//   let json = await _req.json();
-//   const { db } = await connectToDatabase();
-//   const collection = db.collection('Person');
-//   let nodes = await db.collection('Person').find({}).toArray();
-//   if(nodes) await collection.deleteMany({})
-//   // fs.readFile(filePath, 'utf8', (err, data) => {
+    const page_str = request.nextUrl.searchParams.get("page");
+    const limit_str = request.nextUrl.searchParams.get("limit");
 
-//   //   if (err) {
-//   //     console.error('Error reading file:', err);
-//   //     return new NextResponse(
-//   //       JSON.stringify({ message: 'Internal Server Error' }),
-//   //       {
-//   //         status: 500,
-//   //         headers: { 'Content-Type': 'application/json' },
-//   //       }
-//   //     );
-//   //   } else {
-//       // try {
-//         // json?.addNodesData.forEach((node: Root) => {
-//         //   nodes.push(node);
-//         // });
-//         // json?.updateNodesData.forEach((node: { id: string | number }) => {
-//         //   const index = nodes.findIndex(
-//         //     (n: { id: string | number }) => n.id === node.id
-//         //   );
-//         //   nodes[index] = node;
-//         // });
-//         // nodes = nodes.filter(
-//         //   (node: { id: string | number }) => node.id !== json?.removeNodeId
-//         // );
-//         // fs.writeFile(filePath, JSON.stringify(nodes), { flag: 'w' }, async (err) => {
-//           // if (err) {
-//           //   return new NextResponse(
-//           //     JSON.stringify({ message: 'Something went wrong', status: 'error' }),
-//           //     {
-//           //       status: 400,
-//           //       headers: { 'Content-Type': 'application/json' },
-//           //     }
-//           //   );
-//           // } else {
-//             console.log({nodes}, '21')
-//             await collection.insertMany(nodes)
-//             let json_response = {
-//               status: "success",
-//               data:nodes
-//             };
-//             return NextResponse.json(json_response);
-//           // }
-//         // });
-//       // } catch (parseErr) {
-//       //   let error_response = {
-//       //     status: "error",
-//       //     message: "Internal Server Error"
-//       //   };
-//       //   console.error('Error parsing JSON:', parseErr);
-//       //   return new NextResponse(JSON.stringify(error_response),
-//       //     {
-//       //       status: 500,
-//       //       headers: { 'Content-Type': 'application/json' },
-//       //     }
-//       //   );
-//       // }
-//   //   }
-//   // });
-// }
+    const page = page_str ? parseInt(page_str, 10) : 1;
+    const limit = limit_str ? parseInt(limit_str, 10) : 10;
+
+    const { todos, results, error } = await getTodos({ page, limit });
+
+    if (error) {
+      throw error;
+    }
+
+    let json_response = {
+      status: "success",
+      results,
+      todos,
+    };
+    return NextResponse.json(json_response);
+  } catch (error: any) {
+    return createErrorResponse(error.message, 500);
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await connectDB();
+
+    const body = await request.json();
+
+    if (!body.title) {
+      return createErrorResponse("Todo must have a title", 400);
+    }
+
+    const { todo, error } = await createTodo(body.title);
+    if (error) {
+      throw error;
+    }
+
+    let json_response = {
+      status: "success",
+      data: {
+        todo,
+      },
+    };
+    return new NextResponse(JSON.stringify(json_response), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    if (error.code === 11000) {
+      return createErrorResponse("Todo with title already exists", 409);
+    }
+
+    return createErrorResponse(error.message, 500);
+  }
+}
